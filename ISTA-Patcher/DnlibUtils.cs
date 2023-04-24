403e78a822a13a34e509a2e18578ce2e14684c84
TautCony <i@tautcony.xyz>
2023-04-24 11:13:21 +0800

    docs: add comment for helper functions

--- ISTA-Patcher/DnlibUtils.cs
+++ ISTA-Patcher/DnlibUtils.cs
@@ -8,6 +8,11 @@ using dnlib.DotNet.Emit;
 
 public static class DnlibUtils
 {
+    /// <summary>
+    /// Get the description of a method.
+    /// </summary>
+    /// <param name="md">Method to get description.</param>
+    /// <returns>Description of the method.</returns>
     public static string DescriptionOf(MethodDef md)
     {
         var sb = new StringBuilder();
@@ -31,11 +36,25 @@ public static class DnlibUtils
         return sb.ToString();
     }
 
+    /// <summary>
+    /// Get the TypeDef of a method.
+    /// </summary>
+    /// <param name="module">Module to get TypeDef.</param>
+    /// <param name="fullName">Full name of the method.</param>
+    /// <returns>TypeDef of the method.</returns>
     public static TypeDef? GetType(this ModuleDef module, string fullName)
     {
         return module.GetTypes().FirstOrDefault(tp => tp.FullName == fullName);
     }
 
+    /// <summary>
+    /// Get the MethodDef of a method from a module.
+    /// </summary>
+    /// <param name="asm">Assembly to get MethodDef.</param>
+    /// <param name="type">Type of the method.</param>
+    /// <param name="name">Name of the method.</param>
+    /// <param name="desc">Description of the method.</param>
+    /// <returns>MethodDef of the method.</returns>
     public static MethodDef? GetMethod(this AssemblyDef asm, string type, string name, string desc)
     {
         var td = asm.Modules.SelectMany(m => m.GetTypes()).FirstOrDefault(tp => tp.FullName == type);
@@ -43,6 +62,11 @@ public static class DnlibUtils
         return td?.Methods.FirstOrDefault(m => m.Name.Equals(name) && DescriptionOf(m).Equals(desc));
     }
 
+    /// <summary>
+    /// Empty the given method.
+    /// </summary>
+    /// <param name="method">Method to empty.</param>
+    /// <exception cref="Exception">Thrown when the method has no body.</exception>
     public static void EmptyingMethod(this MethodDef method)
     {
         var body = method.Body;
@@ -59,6 +83,11 @@ public static class DnlibUtils
         }
     }
 
+    /// <summary>
+    /// Return 0 in the given method.
+    /// </summary>
+    /// <param name="method">Method to return 0.</param>
+    /// <exception cref="Exception">Thrown when the method has no body.</exception>
     public static void ReturnZeroMethod(this MethodDef method)
     {
         var body = method.Body;
@@ -76,6 +105,11 @@ public static class DnlibUtils
         }
     }
 
+    /// <summary>
+    /// Return 1 in the given method.
+    /// </summary>
+    /// <param name="method">Method to return 1.</param>
+    /// <exception cref="Exception">Thrown when the method has no body.</exception>
     public static void ReturnOneMethod(this MethodDef method)
     {
         var body = method.Body;
@@ -93,22 +127,42 @@ public static class DnlibUtils
         }
     }
 
+    /// <summary>
+    /// Return false in the given method.
+    /// </summary>
+    /// <param name="method">Method to return false.</param>
     public static void ReturnFalseMethod(this MethodDef method)
     {
         ReturnZeroMethod(method);
     }
 
+    /// <summary>
+    /// Return true in the given method.
+    /// </summary>
+    /// <param name="method">Method to return true.</param>
     public static void ReturnTrueMethod(this MethodDef method)
     {
         ReturnOneMethod(method);
     }
 
+    /// <summary>
+    /// Find the first instruction with the given opCode and operand.
+    /// </summary>
+    /// <param name="method">Method to find instruction.</param>
+    /// <param name="opCode">OpCode of the instruction.</param>
+    /// <param name="operandName">Operand of the instruction.</param>
+    /// <returns>Instruction with the given opCode and operand.</returns>
     public static Instruction? FindInstruction(this MethodDef method, OpCode opCode, string operandName)
     {
         return method.Body.Instructions.FirstOrDefault(instruction =>
             instruction.OpCode == opCode && (instruction.Operand as IMethodDefOrRef)?.FullName == operandName);
     }
 
+    /// <summary>
+    /// Find the first instruction with the given opCode and operand.
+    /// </summary>
+    /// <param name="method">Method to find instruction.</param>
+    /// <param name="instructions">Instructions to find.</param>
     public static void ReplaceWith(this MethodDef method, IEnumerable<Instruction> instructions)
     {
         method.Body.Instructions.Clear();
