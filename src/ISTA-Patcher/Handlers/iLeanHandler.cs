4fc340797308675f8e2e79b8098b67eaf8b95014
TautCony <i@tautcony.xyz>
2024-12-11 13:20:32 +0800

    feat: improve ilean cipher

--- src/ISTA-Patcher/Handlers/iLeanHandler.cs
+++ src/ISTA-Patcher/Handlers/iLeanHandler.cs
@@ -3,6 +3,7 @@
 
 namespace ISTA_Patcher.Handlers;
 
+using System.Diagnostics.CodeAnalysis;
 using ISTAlter;
 using ISTAlter.Core.iLean;
 using ISTAlter.Utils;
@@ -10,13 +11,15 @@ using Serilog;
 
 public static class iLeanHandler
 {
+    [SuppressMessage("Interoperability", "CA1416", Justification = "SupportedOSPlatform attribute is used")]
     public static async Task<int> Execute(ISTAOptions.ILeanOptions opts)
     {
         Global.LevelSwitch.MinimumLevel = opts.Verbosity;
+        var isSupportedPlatform = OperatingSystem.IsWindows() || OperatingSystem.IsMacOS() || OperatingSystem.IsLinux();
 
         if (opts.ShowMachineInfo)
         {
-            if (OperatingSystem.IsWindows() || OperatingSystem.IsMacOS() || OperatingSystem.IsLinux())
+            if (isSupportedPlatform)
             {
                 Log.Information("MachineGuid: {MachineGuid}", NativeMethods.GetMachineUUID());
                 Log.Information("VolumeSerialNumber: {VolumeSerialNumber}", NativeMethods.GetVolumeSerialNumber());
@@ -29,39 +32,47 @@ public static class iLeanHandler
             return 0;
         }
 
-        if (!string.IsNullOrEmpty(opts.MachineGuid) && !string.IsNullOrEmpty(opts.VolumeSerialNumber))
+        if (string.IsNullOrEmpty(opts.Encrypt) && string.IsNullOrEmpty(opts.Decrypt))
         {
-            Encryption.InitializeMachineInfo(opts.MachineGuid, opts.VolumeSerialNumber);
-        }
-        else
-        {
-            if (OperatingSystem.IsWindows() || OperatingSystem.IsMacOS())
-            {
-                Encryption.InitializeMachineInfo();
-            }
-            else
-            {
-                Log.Error("MachineGuid and VolumeSerialNumber must be initialized on this platform");
-                return -1;
-            }
+            Log.Warning("No operation matched, exiting...");
+            return 1;
         }
 
-        if (opts.Encrypt != null)
+        if (string.IsNullOrEmpty(opts.MachineGuid) && isSupportedPlatform)
+        {
+            Log.Information("MachineGuid is not provided, read from system...");
+            opts.MachineGuid = NativeMethods.GetMachineUUID();
+        }
+
+        if (string.IsNullOrEmpty(opts.VolumeSerialNumber) && isSupportedPlatform)
+        {
+            Log.Information("VolumeSerialNumber is not provided, read from system...");
+            opts.VolumeSerialNumber = NativeMethods.GetVolumeSerialNumber();
+        }
+
+        if (string.IsNullOrEmpty(opts.MachineGuid) || string.IsNullOrEmpty(opts.VolumeSerialNumber))
+        {
+            Log.Error("MachineGuid or VolumeSerialNumber is not available, exiting...");
+            return -1;
+        }
+
+        using var encryption = new iLeanCipher(opts.MachineGuid, opts.VolumeSerialNumber);
+        if (!string.IsNullOrEmpty(opts.Encrypt))
         {
             var content = File.Exists(opts.Encrypt)
                 ? await File.ReadAllTextAsync(opts.Encrypt).ConfigureAwait(false)
                 : opts.Encrypt;
-            var encrypted = Encryption.Encrypt(content);
+            var encrypted = encryption.Encrypt(content);
             Log.Information("Encrypted: \n{Encrypted}\n", encrypted);
             return 0;
         }
 
-        if (opts.Decrypt != null)
+        if (!string.IsNullOrEmpty(opts.Decrypt))
         {
             var content = File.Exists(opts.Decrypt)
                 ? await File.ReadAllTextAsync(opts.Decrypt).ConfigureAwait(false)
                 : opts.Decrypt;
-            var decrypted = Encryption.Decrypt(content);
+            var decrypted = encryption.Decrypt(content);
             Log.Information("Decrypted: \n{Decrypted}\n", decrypted);
             return 0;
         }
---
+++ src/ISTAlter/Core/iLean/iLeanCipher.cs
@@ -0,0 +1,117 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-FileCopyrightText: Copyright 2024 TautCony
+
+namespace ISTAlter.Core.iLean;
+
+using System.Runtime.Versioning;
+using System.Security.Cryptography;
+using System.Text;
+using ISTAlter.Utils;
+using Serilog;
+
+public sealed class iLeanCipher : IDisposable
+{
+    private readonly string machineGuid;
+
+    private readonly string volumeSerialNumber;
+
+    private readonly Aes aesInstance;
+
+    [SupportedOSPlatform("Windows")]
+    [SupportedOSPlatform("macOS")]
+    [SupportedOSPlatform("Linux")]
+    public iLeanCipher()
+    {
+        this.machineGuid = NativeMethods.GetMachineUUID();
+        this.volumeSerialNumber = NativeMethods.GetVolumeSerialNumber();
+        this.aesInstance = InitializeAesProvider(this.machineGuid, this.volumeSerialNumber);
+    }
+
+    public iLeanCipher(string machineGuid, string volumeSerialNumber)
+    {
+        if (machineGuid.Length != 32 || volumeSerialNumber.Length != 8)
+        {
+            throw new InvalidOperationException("MachineGuid and VolumeSerialNumber must be 32 and 8 characters long.");
+        }
+
+        this.machineGuid = machineGuid;
+        this.volumeSerialNumber = volumeSerialNumber;
+        this.aesInstance = InitializeAesProvider(this.machineGuid, this.volumeSerialNumber);
+    }
+
+    internal static Aes InitializeAesProvider(string machineGuid, string volumeSerialNumber)
+    {
+        if (machineGuid == null || volumeSerialNumber == null)
+        {
+            throw new InvalidOperationException("MachineGuid and VolumeSerialNumber must be initialized.");
+        }
+
+        var clientID = ReverseString(machineGuid);
+        var volumeSNr = volumeSerialNumber;
+        var volumeSN = ReverseString(volumeSNr);
+
+        var iv = clientID[..(clientID.Length / 2)];
+        var key = volumeSN + clientID[(clientID.Length / 2)..] + volumeSNr;
+
+        var aes = Aes.Create();
+        aes.Key = Encoding.UTF8.GetBytes(key);
+        aes.IV = Encoding.UTF8.GetBytes(iv);
+
+        return aes;
+    }
+
+    public string Encrypt(string toEncrypt)
+    {
+        try
+        {
+            using var memoryStream = new MemoryStream();
+            var cryptoStream = new CryptoStream(memoryStream, this.aesInstance.CreateEncryptor(), CryptoStreamMode.Write);
+            var bytes = Encoding.UTF8.GetBytes(toEncrypt);
+            cryptoStream.Write(bytes, 0, bytes.Length);
+            cryptoStream.FlushFinalBlock();
+            return Convert.ToBase64String(memoryStream.ToArray());
+        }
+        catch (CryptographicException ex)
+        {
+            Log.Information("MachineGuid: {MachineGuid}, VolumeSerialNumber: {VolumeSerialNumber}", this.machineGuid, this.volumeSerialNumber);
+            Log.Error(ex, "iLean Encryption failed.");
+            throw;
+        }
+    }
+
+    public string Decrypt(string toDecrypt)
+    {
+        try
+        {
+            using var memoryStream = new MemoryStream();
+            var cryptoStream = new CryptoStream(memoryStream, this.aesInstance.CreateDecryptor(), CryptoStreamMode.Write);
+            var bytes = Convert.FromBase64String(toDecrypt);
+            cryptoStream.Write(bytes, 0, bytes.Length);
+            cryptoStream.FlushFinalBlock();
+            return Encoding.UTF8.GetString(memoryStream.ToArray());
+        }
+        catch (CryptographicException ex)
+        {
+            Log.Information("MachineGuid: {MachineGuid}, VolumeSerialNumber: {VolumeSerialNumber}", this.machineGuid, this.volumeSerialNumber);
+            Log.Error(ex, "iLean Decryption failed.");
+            throw;
+        }
+    }
+
+    private static string ReverseString(string value)
+    {
+        if (string.IsNullOrEmpty(value))
+        {
+            return string.Empty;
+        }
+
+        var array = value.ToCharArray();
+        Array.Reverse(array);
+        return new string(array);
+    }
+
+    public void Dispose()
+    {
+        this.aesInstance.Dispose();
+    }
+}
